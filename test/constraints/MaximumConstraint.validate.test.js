a0cf22d5a40a0f4bb078c16edf540aeb9e6c387b
Simon Bull <simon.bull.84@googlemail.com>
2019-02-25 11:35:57 +0000

    Add maximum constraint

---
+++ test/constraints/MaximumConstraint.validate.test.js
@@ -0,0 +1,41 @@
+const MaximumConstraint = require('./../../lib/contraints/MaximumConstraint');
+
+describe('when validating selected roles against maximum constraint', () => {
+  it('then it should not return exceptions if maximum number is met', () => {
+    const selections = ['role-1', 'role-2', 'role-3'];
+    const constraint = new MaximumConstraint(3);
+
+    const actual = constraint.validate(selections);
+
+    expect(actual).toBeDefined();
+    expect(actual).toBeInstanceOf(Array);
+    expect(actual).toHaveLength(0);
+  });
+
+  it('then it should not return exceptions if maximum number is not reached', () => {
+    const selections = ['role-1', 'role-2'];
+    const constraint = new MaximumConstraint(3);
+
+    const actual = constraint.validate(selections);
+
+    expect(actual).toBeDefined();
+    expect(actual).toBeInstanceOf(Array);
+    expect(actual).toHaveLength(0);
+  });
+
+  it('then it should return exception if maximum number is exceeded', () => {
+    const selections = ['role-1', 'role-2', 'role-3', 'role-4'];
+    const constraint = new MaximumConstraint(3);
+
+    const actual = constraint.validate(selections);
+
+    expect(actual).toBeDefined();
+    expect(actual).toBeInstanceOf(Array);
+    expect(actual).toHaveLength(1);
+    expect(actual[0]).toMatchObject({
+      constraint: 'MaximumConstraint',
+      message: 'Expected a maximum of 3 roles to be selected but received 4 selected roles',
+      appliesTo: ['role-1', 'role-2', 'role-3', 'role-4'],
+    });
+  });
+});
