6677c5dd065de1fdad6b6824faf061b9ed72c6ef
Simon Bull <simon.bull.84@googlemail.com>
2019-02-25 11:30:03 +0000

    Add minimum constraint

---
+++ test/constraints/MinimumConstraint.validate.test.js
@@ -0,0 +1,41 @@
+const MinimumConstraint = require('./../../lib/contraints/MinimumConstraint');
+
+describe('when validating selected roles against minimum constraint', () => {
+  it('then it should not return exceptions if minimum number is met', () => {
+    const selections = ['role-1', 'role-2'];
+    const constraint = new MinimumConstraint(2);
+
+    const actual = constraint.validate(selections);
+
+    expect(actual).toBeDefined();
+    expect(actual).toBeInstanceOf(Array);
+    expect(actual).toHaveLength(0);
+  });
+
+  it('then it should not return exceptions if minimum number is exceeded', () => {
+    const selections = ['role-1', 'role-2', 'role-3'];
+    const constraint = new MinimumConstraint(2);
+
+    const actual = constraint.validate(selections);
+
+    expect(actual).toBeDefined();
+    expect(actual).toBeInstanceOf(Array);
+    expect(actual).toHaveLength(0);
+  });
+
+  it('then it should return exception if minimum number if not met', () => {
+    const selections = ['role-1', 'role-2'];
+    const constraint = new MinimumConstraint(3);
+
+    const actual = constraint.validate(selections);
+
+    expect(actual).toBeDefined();
+    expect(actual).toBeInstanceOf(Array);
+    expect(actual).toHaveLength(1);
+    expect(actual[0]).toMatchObject({
+      constraint: 'MinimumConstraint',
+      message: 'Expected a minimum of 3 roles to be selected but received 2 selected roles',
+      appliesTo: ['role-1', 'role-2'],
+    });
+  });
+});
