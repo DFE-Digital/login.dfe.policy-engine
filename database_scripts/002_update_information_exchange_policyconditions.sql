d1c3d83b4df32f4e3cb1775c7364d39e672d1cba
DrogoNevets <steve.colemanwilliams@gmail.com>
2020-03-18 16:04:24 +0000

    updated scripts and location

---
+++ database_scripts/002_update_information_exchange_policyconditions.sql
@@ -0,0 +1,20 @@
+BEGIN TRAN AdditionalPolicy
+ DECLARE @policyid uniqueidentifier;
+ SET @policyid = newid();
+    INSERT INTO Policy (Id, Name, ApplicationId, Status, CreatedAt, UpdatedAt)
+    VALUES (@policyid, 'EFA Information Exchange - FE/TP/OSH/EYS', '913ba321-9547-46b2-93c3-a7a7ffc2e3e2', 1, getdate(), getdate());
+    INSERT INTO PolicyCondition (Id, PolicyId, Field, Operator, Value, CreatedAt, UpdatedAt)
+    VALUES (newid(), @policyid, 'organisation.category.id', 'is', '009', getdate(), getdate());
+    INSERT INTO PolicyCondition (Id, PolicyId, Field, Operator, Value, CreatedAt, UpdatedAt)
+    VALUES (newid(), @policyid, 'organisation.category.id', 'is', '051', getdate(), getdate());
+    INSERT INTO PolicyCondition (Id, PolicyId, Field, Operator, Value, CreatedAt, UpdatedAt)
+    VALUES (newid(), @policyid, 'organisation.category.id', 'is', '008', getdate(), getdate());
+    INSERT INTO PolicyCondition (Id, PolicyId, Field, Operator, Value, CreatedAt, UpdatedAt)
+    VALUES (newid(), @policyid, 'organisation.category.id', 'is', '004', getdate(), getdate());
+    INSERT INTO PolicyCondition (Id, PolicyId, Field, Operator, Value, CreatedAt, UpdatedAt)
+    VALUES (newid(), @policyid, 'organisation.status.id', 'is', '1', getdate(), getdate());
+    INSERT INTO PolicyCondition (Id, PolicyId, Field, Operator, Value, CreatedAt, UpdatedAt)
+    VALUES (newid(), @policyid, 'organisation.status.id', 'is', '3', getdate(), getdate());
+    INSERT INTO PolicyCondition (Id, PolicyId, Field, Operator, Value, CreatedAt, UpdatedAt)
+    VALUES (newid(), @policyid, 'organisation.status.id', 'is', '4', getdate(), getdate());
+ROLLBACK TRAN AdditionalPolicy
