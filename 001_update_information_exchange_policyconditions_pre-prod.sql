31d92a0b823025e32a4c7be67fa8d2beb1f6cb1f
DrogoNevets <steve.colemanwilliams@gmail.com>
2020-03-18 13:46:43 +0000

    added scripts

---
+++ 001_update_information_exchange_policyconditions_pre-prod.sql
@@ -0,0 +1,10 @@
+BEGIN TRAN AdditionalPolicy
+ DECLARE @policyid uniqueidentifier;
+ SET @policyid = newid();
+    INSERT INTO Policy (Id, Name, ApplicationId, Status, CreatedAt, UpdatedAt)
+    VALUES (@policyid, 'New policy name here', '913ba321-9547-46b2-93c3-a7a7ffc2e3e2', 1, getdate(), getdate());
+    INSERT INTO PolicyCondition (Id, PolicyId, Field, Operator, Value, CreatedAt, UpdatedAt)
+    VALUES (newid(), @policyid, 'organisation.category.id', 'is', '009', getdate(), getdate());
+    INSERT INTO PolicyCondition (Id, PolicyId, Field, Operator, Value, CreatedAt, UpdatedAt)
+    VALUES (newid(), @policyid, 'organisation.category.id', 'is', '051', getdate(), getdate());
+ROLLBACK TRAN AdditionalPolicy
