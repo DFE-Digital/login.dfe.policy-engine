8a31918696b4ebe8324fb34ca0a92d0ff01fbda3
jovin-joy <jovin.joy@education.gov.uk>
2020-03-19 16:56:17 +0000

    adding backoffice user

--- database_scripts/003_add_backoffice_role_user.sql
+++ database_scripts/003_add_backoffice_role_user.sql
@@ -3,4 +3,10 @@ BEGIN TRAN addBackofficeUser
 UpdatedAt)
     VALUES (newid(), '88df18ad-7a0f-4617-ba76-13f48ae3149e', 'id', 'is', '9C961696-CBE4-40A9-B65C-847AD2EC75B6', getdate(),
 getdate());
+
+    INSERT INTO PolicyCondition (Id, PolicyId, Field, Operator, Value, CreatedAt,
+UpdatedAt)
+    VALUES (newid(), '88df18ad-7a0f-4617-ba76-13f48ae3149e', 'id', 'is', '449F1910-8381-4CFE-B411-6C13273A331B', getdate(),
+getdate());
+
 ROLLBACK TRAN addBackofficeUser
